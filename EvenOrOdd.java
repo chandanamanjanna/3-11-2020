ad497fec65e37c51b0115619dc4978f486903bc1
chandanamanjanna <71077851+chandanamanjanna@users.noreply.github.com>
2020-11-03 12:51:36 +0530

    Add files via upload

---
+++ EvenOrOdd.java
@@ -0,0 +1,13 @@
+class EvenOrOdd{
+	public static void main(String args[]){
+	int num[]={99,88,77,66,55,44,33,22,11,10,9,8,7,6,};
+	for(int i=0;i<15;i++){
+	if(num[i]%2==0){
+	System.out.println(num[i] +": is even");
+	}
+	else{
+	System.out.println(num[i] +": is odd");
+	}
+	}
+}
+}
