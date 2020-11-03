d29c2a68f5272b7dafb9dfc5e0ffed7ebee906d6
chandanamanjanna <71077851+chandanamanjanna@users.noreply.github.com>
2020-11-03 12:49:22 +0530

    Add files via upload

---
+++ EvenIndexValue.java
@@ -0,0 +1,10 @@
+class EvenIndexValue{
+	public static void main(String args[]){
+	int values[]={1,4,55,62,88,9,71,66,8,4};
+	for(int i=0;i<10;i++){
+	if(i%2==0){
+	System.out.println(values[i]);
+	}
+	}
+}
+}
