6cfc39baf37be083692dad6e71617c538b8c1def
chandanamanjanna <71077851+chandanamanjanna@users.noreply.github.com>
2020-11-03 12:48:57 +0530

    Add files via upload

---
+++ Division.java
@@ -0,0 +1,10 @@
+class Division{
+	public static void main(String args[]){
+	int numbers[]={11,22,33,44,55,66,77,88,99};
+	for(int i=0;i<=9;i++){
+	if(numbers[i]%2==0){
+	System.out.println(numbers[i]);
+	}
+	}
+}
+}
